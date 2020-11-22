f3495062339986e72cc7204c16f00a7230b7f943
João Marcelo <62082022+honesttea@users.noreply.github.com>
2020-11-21 22:43:56 -0300

    Update arquivo3.c

--- arquivo3.c
+++ arquivo3.c
@@ -4,16 +4,17 @@
 
 int main(){
     FILE * loucura;
-    int t,i;
+    int t=0,i;
     char streamzinha[1000];
 
     loucura = fopen("nomes.txt","r");
     for(i=0;fgets(streamzinha,1000,loucura)!=NULL;i++){
         if(streamzinha[0]=='b'||streamzinha[0]=='B'){
-            printf("%s",streamzinha);
+            t++;
         }
     }
-
+    
+    printf("No arquivo possui %d pessoas com o nome que inicia com a letra B.",t);
     fclose(loucura);
 
     return 0;
